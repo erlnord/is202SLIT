731f9b42cdb24f0e102afbd86a8b2f86f07ede3d
lmgr <lmgr@DESKTOP-Q5ENO98>
2015-11-16 15:37:05 +0100

    nyx nyx nyx

--- Downloads/SLIT 2/SLIT-client/src/java/slitclient/Main.java
+++ Downloads/SLIT 2/SLIT-client/src/java/slitclient/Main.java
@@ -6,6 +6,7 @@
 package slitclient;
 
 import java.awt.Color;
+import java.util.Properties;
 import javax.ejb.EJB;
 import javax.swing.*;
 import service.messageBeanRemote;
@@ -22,38 +23,39 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         
         //Boolean result = messageInstance.addMessage("Dette er en test");
         //System.out.println("Result: " +result);
         
         // TODO code application logic here
+        
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // Log-in Window
+        
         guiLogInWindow logIn = new guiLogInWindow();
         logIn.setVisible(true);
-        logIn.setSize(400,150);
+        logIn.setSize(600,150);
         logIn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         logIn.setBackground(Color.WHITE);
         // Plasserer applikasjonen midt på skjermen
         logIn.setLocationRelativeTo(null);
-        
-        
-        }
+
+    }
     
     public static void studentMainView() {
         // StudentMainView
-            StudentMainView smw = new StudentMainView();
-            smw.frame.setVisible(true);
-            smw.frame.setSize(1024,768);
-            smw.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            smw.frame.setBackground(Color.WHITE);
-            smw.frame.setLocationRelativeTo(null);
-        }
+        StudentMainView smw = new StudentMainView();
+        smw.frame.setVisible(true);
+        smw.frame.setSize(1024,768);
+        smw.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        smw.frame.setBackground(Color.WHITE);
+        smw.frame.setLocationRelativeTo(null);
+    }
     
     public static void teacherMainView() {
         //TeacherMainView
@@ -66,7 +68,7 @@ public class Main {
     }
         
 
-    }
+}
         
         
         
